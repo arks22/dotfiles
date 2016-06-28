16cf6cdca281c3bb6200361efb553874fc6195c6
arks22 <sasaki.10222022@gmail.com>
2016-06-28 21:09:01 +0900

    う

--- etc/install.sh
+++ etc/install.sh
@@ -4,4 +4,4 @@ ln -s -f ~/dotfiles/vim/.vimrc ~
 ln -s -f ~/dotfiles/vim/.vim ~
 ln -s -f ~/dotfiles/tmux/.tmux.conf ~
 ln -s -f ~/dotfiles/zsh/.zshrc ~
-ln -s -f ~/dotfiles/zsh/dircolors.ansi-universal ~/dircolors
+ln -s -f ~/dotfiles/zsh/dircolors ~
