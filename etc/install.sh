51a7fdfd8d88ef32b03e8e029c96ac400c57d994
arks22 <sasaki.10222022@gmail.com>
2016-06-28 21:45:24 +0900

    グレゴリオ暦で年始から179日目（閏年では180日目）にあたり、年末まであと186日ある

--- etc/install.sh
+++ etc/install.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-ln -s -f ~/dotfiles/vim/.vimrc ~
-ln -s -f ~/dotfiles/vim/.vim ~
-ln -s -f ~/dotfiles/tmux/.tmux.conf ~
-ln -s -f ~/dotfiles/zsh/.zshrc ~
-ln -s -f ~/dotfiles/zsh/dircolors ~
+files=( vim/.vimrc vim/.vim tmux/.tmux.conf zsh/.zshrc zsh/dircolors )
+
+for file in ${files[@]}; do
+  ln -s -f ~/dotfiles/$file ~
+done
