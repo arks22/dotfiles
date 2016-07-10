02ad6c901c5bb7f81638e50c67445abb9ba153c5
arks22 <sasaki.10222022@gmail.com>
2016-07-10 17:43:22 +0900

    [update] zshrc/etc.zsh ちょっと

--- zshrc/etc.zsh
+++ zshrc/etc.zsh
@@ -4,6 +4,8 @@ autoload -Uz add-zsh-hook
 autoload -U colors
 eval $(gdircolors ~/dircolors)
 
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="20"
+
 #補完候補でもLS_COLORSを使う
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
