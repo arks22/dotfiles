503e581a68e1d72530baf17c025c1c0d17632b31
arks22 <sasaki.10222022@gmail.com>
2016-07-28 17:03:12 +0900

    [improve] zsh/zshrc/03_prompt.zsh

--- zsh/zshrc/03_prompt.zsh
+++ zsh/zshrc/03_prompt.zsh
@@ -3,17 +3,18 @@
 #excute before display prompt
 function precmd() {
   if git_info=$(git status 2>/dev/null ); then
-    [[ $git_info =~ "Changes not staged" ]] &&  git_unstaged=" ±" || git_unstaged=""
-    [[ $git_info =~ "Changes to be committed" ]] && git_uncommited=" ●" || git_uncommited=""
-    [ -z "${git_unstaged}${git_uncommited}" ] && git_clean=" ✔ " || git_clean=""
+    [[ $git_info =~ "Changes not staged" ]] &&  git_unstaged="%K{blue}%F{black} ± %f%k" || git_unstaged=""
+    [[ $git_info =~ "Changes to be committed" ]] && git_uncommited="%{[30;48;5;013m%}%F{black} • %k%f" || git_uncommited=""
+    [ -z "${git_unstaged}${git_uncommited}" ] && git_clean="%K{green}%F{black} ✔ %f%k" || git_clean=""
 
     git_branch="$(echo $git_info | awk 'NR==1 {print $3}')"
-    git_info="%K{black} ${git_branch} %K{blue}%F{black}${git_unstaged}${git_uncommited} %k%K{green}${git_clean}%f%k"
+    git_info="%K{black} ${git_branch} ${git_unstaged}${git_uncommited}${git_clean}"
   fi
   [ $(whoami) = "root" ] && root="%K{black}%F{yellow} ⚡ %{[38;5;010m%}│%f%k"
   dir_info=$dir
   dir="%F{cyan}%K{black} %~ %k%f"
 }
+
 
 dir="%F{cyan}%K{black} %~ %k%f"
 
