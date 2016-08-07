5e6016bfc809ccf8fc41a71becf3ed500f10815f
arks22 <sasaki.10222022@gmail.com>
2016-08-08 01:34:57 +0900

    improve:[ 06_tmux.zsh ]

--- zsh/zshrc/06_tmux.zsh
+++ zsh/zshrc/06_tmux.zsh
@@ -81,15 +81,6 @@ tmux_kill_window_choices() {
 
 
 if [ ! -z $TMUX ]; then
-  echo "–––––––––––––––––– ${fg[blue]}tmux windows${reset_color} ––––––––––––––––––"
-  tmux list-windows | while read line; do
-    if [[ $line =~ "active" ]]; then
-      echo "${fg[yellow]}*${reset_color} $(echo $line | awk '{print $1 " " $2 " " $3 " " $4 " " $5}')"
-    else
-      echo "  $(echo $line | awk '{print $1 " " $2 " " $3 " " $4 " " $5}')"
-    fi
-  done
-  echo "––––––––––––––––––––––––––––––––––––––––––––––––––"
   echo "– – – – – – – – – – – ${fg_bold[red]}TMUX${reset_color} – – – – – – – – – – – –"
 else
   tmux_operation
