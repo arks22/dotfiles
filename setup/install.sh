7399487fa78e1859082e042cf44f044aa5924dd2
arks22 <sasaki.10222022@gmail.com>
2016-08-18 15:39:49 +0900

    improve:[ install.sh ]

--- setup/install.sh
+++ setup/install.sh
@@ -1,15 +1,18 @@
 #!bin/sh
 
 if which git >/dev/null 2>&1; then
+  echo "Downloading dotfiles with git..."
   git clone https://github.com/arks22/dotfiles.git ~/dotfiles
 else
   tarball="https://github.com/arks22/dotfiles/archive/master.tar.gz"
   if which curl >/dev/null 2>&1; then
-    curl -L "$tarball"
+    echo "Downloading dotfiles with curl..."
+    curl -L "$tarball" | tar xv -
   elif which wget >/dev/null 2>&1; then
-    wget -O - "$tarball"
-  fi | tar xv -
+    echo "Downloading dotfiles with wget..."
+    wget -O - "$tarball" | tar xv -
+  fi
   mv -f dotfiles-master ~/dotfiles
 fi
 
-sh ~/dotfiles/setup/init.sh
+#sh ~/dotfiles/setup/init.sh
