b4a985adc4fbe5972cfdd8e175c63171aa50a2ac
arks22 <sasaki.10222022@gmail.com>
2016-08-18 16:21:26 +0900

    improve:[ init.sh install.sh ]

--- setup/init.sh
+++ setup/init.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
+source ~/dotfiles/setup/symlink.sh
+
 #install brew
 if ! which brew; then
   if [ $(uname -s) == "Darwin" ]; then
     echo "Installing Homebrew ..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew tap homebrew/brewdler
+    brew brewdle
   fi
 fi
 
 fpath=(~/dotfiles/bin(N-/) $fpath)
-
-source ~/dotfiles/setup/symlink.sh
-
-brew tap homebrew/brewdler
-brew brewdle
 
 [ $SHELL = "/bin/zsh" ] || chsh -s /bin/zsh
 zsh
--- setup/install.sh
+++ setup/install.sh
@@ -15,4 +15,4 @@ else
   mv -f dotfiles-master ~/dotfiles
 fi
 
-#sh ~/dotfiles/setup/init.sh
+sh ~/dotfiles/setup/init.sh
