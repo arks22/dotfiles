77eac58a96176e2150e38b738884b27b122736c4
arks22 <sasaki.10222022@gmail.com>
2018-03-27 22:42:12 +0900

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -3,7 +3,7 @@
 if [ ! -e $HOME/dotfiles ]; then
   if type git >/dev/null 2>&1; then
     echo "Downloading dotfiles with git..."
-    git clone https://github.com/arks22/dotfiles.git ~/dotfiles
+    git clone https://github.com/arks22/dotfiles.git $HOME/dotfiles
   else
     tarball="https://github.com/arks22/dotfiles/archive/master.tar.gz"
     if type curl >/dev/null 2>&1; then
@@ -20,8 +20,5 @@ else
   echo "If you want to install arks22/dotfiles, you need to delete ( or move ) your ~/dotfiles."
 fi
 
-chmod +x ~/dotfiles/commands/dotmanager
-ln -s ~/dotfiles/commands/dotmanager /usr/local/bin/dotmanager
-
-dotmanager link
-dotmanager init
+bash $HOME/dotfiles/commands/dotmanager link
+bash $HOME/dotfiles/commands/dotmanager init
