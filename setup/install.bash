94e76635a15859612ff276a8f8d0157a8b5d8391
arks22 <sasaki.10222022@gmail.com>
2018-04-21 15:03:17 +0900

    improve:[ dotmanager, install.bash ]

--- setup/install.bash
+++ setup/install.bash
@@ -13,8 +13,9 @@ if [ ! -e $HOME/dotfiles ]; then
     fi
     mv -f dotfiles-master ~/dotfiles
   fi
-  bash $HOME/dotfiles/bin/dotmanager deploy
-  bash $HOME/dotfiles/bin/dotmanager init
+  source $HOME/dotfiles/bin/dotmanager >/dev/null 2>&1
+  dotmanager::deploy
+  dotmanager::init
 else
   echo "dotfiles are already exists."
   echo "If you want to install arks22/dotfiles, you need to delete ( or move ) your ~/dotfiles."
